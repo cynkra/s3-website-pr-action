51c7c275106befe3dbc0998b45b0cc418bcc3335
Wael Sadek <wael.sadek@gmail.com>
2023-01-20 16:46:00 +0200

    update aws region

--- src/actions/prUpdatedAction.ts
+++ src/actions/prUpdatedAction.ts
@@ -14,7 +14,7 @@ export const requiredEnvVars = [
 ];
 
 export default async (bucketName: string, uploadDirectory: string, environmentPrefix: string) => {
-  const awsRegion = process.env['AWS_DEFAULT_REGION'] ? process.env['AWS_DEFAULT_REGION'] : 'eu-east-1';
+  const awsRegion = process.env['AWS_REGION'] ? process.env['AWS_REGION'] : 'eu-east-1';
   const websiteUrl = `http://${bucketName}.s3-website-${awsRegion}.amazonaws.com`;
   const { repo } = github.context;
   const branchName = github.context.payload.pull_request!.head.ref;
