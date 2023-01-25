6ad55e53f10c713eac29765f776604130f5f6fc7
Wael Sadek <wael.sadek@gmail.com>
2023-01-25 21:32:25 +0200

    update package.json

--- src/githubClient.ts
+++
@@ -1,5 +0,0 @@
-import * as github from '@actions/github';
-
-const { GITHUB_TOKEN } = process.env;
-
-export default github.getOctokit(GITHUB_TOKEN, { previews: ['ant-man-preview', 'flash-preview'] });
--- src/utils/deleteDeployments.ts
+++ src/utils/deleteDeployments.ts
@@ -1,5 +1,5 @@
 import * as github from "@actions/github";
-import githubClient from '../githubClient';
+import octokit from '../octokit';
 
 export default async (
     repo: {
@@ -10,35 +10,47 @@ export default async (
 ) => {
     const environment = `${environmentPrefix || 'PR-'}${github.context.payload.pull_request!.number}`;
 
-    const deployments = await githubClient.graphql(`
-      query GetDeployments($owner: String!, $repo: String!, $environments: [String!]) {
-        repository(owner: $owner, name: $repo) {
-          deployments(first: 100, environments: $environments) {
-            nodes {
-              id
-            }
-          }
-        }
-      }`, { ...repo, environments: [environment] })
+    const deployments = await octokit.repos.listDeployments({
+      ...repo,
+      environment: environment
+    });
 
-    const nodes = deployments.repository?.deployments?.nodes;
+    // const deployments = await octokit.graphql(`
+    //   query GetDeployments($owner: String!, $repo: String!, $environments: [String!]) {
+    //     repository(owner: $owner, name: $repo) {
+    //       deployments(first: 100, environments: $environments) {
+    //         nodes {
+    //           id
+    //         }
+    //       }
+    //     }
+    //   }`, { ...repo, environments: [environment] })
+
+    // const nodes = deployments.repository?.deployments?.nodes;
 
     console.log(JSON.stringify(deployments))
 
-    if (nodes.length <= 0) {
-        console.log(`No exiting deployments found for pull request`);
-        return;
-    }
+    for (const deployment of deployments.data) {  
+      await octokit.repos.deleteDeployment({
+        ...repo,
+        deployment_id: deployment.id
+      })
+    };
+    
+    // if (nodes.length <= 0) {
+    //     console.log(`No exiting deployments found for pull request`);
+    //     return;
+    // }
 
-    for (const node of nodes) {
-        console.log(`Deleting existing deployment - ${node.id}`);
+    // for (const node of nodes) {
+    //     console.log(`Deleting existing deployment - ${node.id}`);
 
-        await githubClient.graphql(`
-          mutation DeleteDeployment($id: ID!) {
-            deleteDeployment(input: {id: $id} ) {
-              clientMutationId
-            }
-          }
-        `, { id: node.id })
-    }
+    //     await octokit.graphql(`
+    //       mutation DeleteDeployment($id: ID!) {
+    //         deleteDeployment(input: {id: $id} ) {
+    //           clientMutationId
+    //         }
+    //       }
+    //     `, { id: node.id })
+    // }
 }
